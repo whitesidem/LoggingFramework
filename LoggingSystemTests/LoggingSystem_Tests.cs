2638a0bcc4982e28d16d0d491a103644d05bcf98
Michael Whiteside <m.whiteside@blueyonder.co.uk>
2014-03-02 19:37:22 +0000

    Place Nlog behind framework so easier to replace if required

--- LoggingSystemTests/LoggingSystem_Tests.cs
+++ LoggingSystemTests/LoggingSystem_Tests.cs
@@ -1,64 +1,113 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using LoggingSystem;
+using LoggingSystem.Interfaces;
+using LoggingSystem.LogPublisher;
+using LoggingSystem.Models;
+using Moq;
 using NLog;
 using NUnit.Framework;
-using Slf;
-using Slf.BitFactoryFacade;
-using Slf.Factories;
-
 namespace LoggingSystemTests
 {
 
     [TestFixture]
 // ReSharper disable InconsistentNaming
-    public class LoggingSystem_Tests
+    public class LoggingSystem_Tests : BaseLogPublisher
     {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private string _loggedMessage;
+        private DateTime ControlledDateTime = DateTime.Now;
 
-
-        [Test]
-        public void WhenLogging_CanGetCallingMethod()
+        public LoggingSystem_Tests(): base(logger)
         {
-
-            //Act
-            var callingMethod = TraceStubCall("Logged output");
-            //Assert
-            Assert.That(callingMethod, Is.EqualTo("WhenLogging_CanGetCallingMethod"));
-
+            
         }
 
-        private string TraceStubCall(string loggedOutput, [CallerMemberName] string caller = "")
+
+        public LoggingSystem_Tests(Logger logger) : base(logger)
         {
-            Assert.That(loggedOutput, Is.Not.Null);
-            return caller;
         }
+
 
         [SetUp]
         public void SetUp()
         {
+            LogSystem.AmbientDate =  DateTime.Parse("2014-03-02T19:16:02.5050832+00:00"); 
+            _loggedMessage = null;
         }
 
         [TearDown]
         public void TearDown()
         {
+            LogSystem.AmbientDate = null;
         }
 
-        //[Test]
-        //public void BitFactoryLoggingExplicitly()
-        //{
-        //    var logger = BitFactoryLogger.CreateRollingFileLogger(@"c:\log\log_{0}.txt", 1000);
-        //    logger.Info("Hello New Logger");
-        //    logger.Info("Hello2 New Logger");
-        //    logger.Info("Hello3 New Logger {0}, and {1}", "ABC", "DEF");
-        //}
+
+        [Test, Category("Implementation Test")]
+        public void NLog_DirectCall_Test()
+        {
+            //Check result in Sentinal.exe
+            logger.Trace("Another test 2");
+        }
+
+        [Test, Category("Integration Test")]
+        public void NLog_DirectCall_JsonTest()
+        {
+            var example =
+                @"{'timestamp': '2014-03-02T19:16:02.5050832+00:00','facility': 'clientSide','clientip': '123.123.123.123','domain': 'www.example.com','server': 'abc-123','request': '/page/request','pagename': 'funnel:example com:page1','searchKey': '1234567890_','sessionID': '11111111111111','event1': 'loading','event2': 'interstitial display banner','severity': 'WARN','short_message': '....meaning short message for aggregation...','full_message': 'full LOG message','userAgent': '...blah...blah..blah...','RT': 2}";
+            example = example.Replace("'", "\"");
+
+            //Check result in Sentinal.exe
+            logger.Info(example);
+        }
+
+        [Test, Category("Integration Test")]
+        public void NLogJsonFormatTest()
+        {
+            //Check result in Sentinal.exe
+
+
+        //    logger.Info(example);
+        }
 
         [Test]
-        public void NLogTest()
+        public void Log_GivenMockPublisher_LogsMessageAsJson()
         {
-            logger.Trace("Sample informational message");
+            var logPublisherFactoryMock = new Mock<ILogPublisherFactory>();
+            logPublisherFactoryMock.Setup(m => m.CreatePublisher(It.IsAny<LoggingLevel>(), Logger)).Returns(this);
+
+
+            var TestGuid =  Guid.Empty;
+            var loggingSystm = new LogSystem(TestGuid, logPublisherFactoryMock.Object);
+
+            string testMessage1 = "Test Message1";
+
+            //Act
+            loggingSystm.Log(Logger, LoggingLevel.Debug, testMessage1, null);
+
+            //Assert
+            Assert.That(_loggedMessage, Is.StringContaining(testMessage1));
+
+            const string expectedMessage =
+                "{\"DateTime\":\"2014-03-02T19:16:02.5050832+00:00\",\"Method\":\"Log_GivenMockPublisher_LogsMessageAsJson\",\"UserId\":\"00000000-0000-0000-0000-000000000000\",\"Message\":\"Test Message1\"}";
+            Assert.That(_loggedMessage, Is.EqualTo(expectedMessage));
 
         }
 
 
+        public override bool IsLogLevelEnabled
+        {
+            get { return true; }
+        }
 
+        public override void LogMessage(string message)
+        {
+            _loggedMessage = message;
+        }
+
+        public override void LogException(string message, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
