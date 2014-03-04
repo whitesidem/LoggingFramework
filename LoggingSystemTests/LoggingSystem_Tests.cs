336c2bd1ecc400a71f88b4fb1b290a433d4fa40d
Michael Whiteside <m.whiteside@blueyonder.co.uk>
2014-03-04 21:52:16 +0000

    include exception detail, line number, log type and path in json

--- LoggingSystemTests/LoggingSystem_Tests.cs
+++ LoggingSystemTests/LoggingSystem_Tests.cs
@@ -16,11 +16,13 @@ namespace LoggingSystemTests
     {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         private readonly List<string> _loggedMessages = new List<string>();
+        private readonly List<string> _loggedExceptionMessages = new List<string>();
         private readonly Guid TestGuid = Guid.Empty;
         private Mock<ILogPublisherFactory> _logPublisherFactoryMock;
         private LogSystem _loggingSystm;
         private const string TestMessage1 = "Test Message1";
         private const string TestDateTimeString = "2014-03-02T19:16:02.5050832+00:00";
+        private bool _isLogEnabled = true;
 
         public LoggingSystem_Tests(): base(logger)
         {
@@ -36,8 +38,10 @@ namespace LoggingSystemTests
         [SetUp]
         public void SetUp()
         {
+            _isLogEnabled = true;
             LogSystem.AmbientDate =  DateTime.Parse(TestDateTimeString); 
             _loggedMessages.Clear();
+            _loggedExceptionMessages.Clear();
             _logPublisherFactoryMock = new Mock<ILogPublisherFactory>();
             _logPublisherFactoryMock.Setup(m => m.CreatePublisher(It.IsAny<LoggingLevel>(), Logger)).Returns(this);
             _loggingSystm = new LogSystem(TestGuid, _logPublisherFactoryMock.Object);
@@ -65,17 +69,40 @@ namespace LoggingSystemTests
             example = example.Replace("'", "\"");
 
             //Check result in Sentinal.exe
-            logger.Info(example);
+            logger.Trace(example);
         }
 
         [Test, Category("Integration Test")]
         public void NLogJsonFormatTest()
         {
-            //Check result in Sentinal.exe
+            LogSystem.AmbientDate = null;
 
+            //Check result in Sentinal.exe and Kibana viewer
+            var loggingSystm = new LogSystem(TestGuid, new LogPublisherFactory());
 
-        //    logger.Info(example);
+            //Act
+            loggingSystm.Log(Logger, LoggingLevel.Info, TestMessage1, null);
+            loggingSystm.Log(Logger, LoggingLevel.Info, TestMessage1, new KeyValueCollection(new KeyValue("Age", "9"), new KeyValue("Year", "2014")));
+            loggingSystm.Log(Logger, LoggingLevel.Info, TestMessage1, new KeyValueCollection(new KeyValue("Age", "9"), new KeyValue("Answer", null), new KeyValue("Year", "2014")));
         }
+
+        [Test, Category("Integration Test")]
+        public void ExceptionTest()
+        {
+            LogSystem.AmbientDate = null;
+
+            //Check result in Sentinal.exe and Kibana viewer
+            var loggingSystm = new LogSystem(TestGuid, new LogPublisherFactory());
+
+            Exception testException = CreateNestedException();
+
+            //Act
+            loggingSystm.Log(testException, Logger, LoggingLevel.Error, TestMessage1, null);
+            loggingSystm.Log(testException, Logger, LoggingLevel.Error, TestMessage1, new KeyValueCollection(new KeyValue("Age", "9"), new KeyValue("Year", "2014")));
+            loggingSystm.Log(testException, Logger, LoggingLevel.Error, TestMessage1, new KeyValueCollection(new KeyValue("Age", "9"), new KeyValue("Answer", null), new KeyValue("Year", "2014")));
+        }
+
+
 
         [Test]
         public void Log_GivenMockPublisherWithNoParams_LogsCorrectMessage()
@@ -90,8 +117,10 @@ namespace LoggingSystemTests
             
             Assert.That(_loggedMessages[0], Is.StringContaining(TestMessage1));
 
-            string expectedMessage = String.Format("{{\"DateTime\":\"{0}\",\"Method\":\"{1}\",\"UserId\":\"00000000-0000-0000-0000-000000000000\",\"Message\":\"Test Message1\"}}", TestDateTimeString ,"Log_GivenMockPublisherWithNoParams_LogsCorrectMessage");
-            Assert.That(_loggedMessages[0], Is.EqualTo(expectedMessage));
+            string expectedMessageContains1 = String.Format("{{\"LogType\":\"Debug\",\"DateTime\":\"{0}\",\"Method\":\"{1}\",\"LineNumber\":", TestDateTimeString, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            const string expectedMessageContains2 = ",\"UserId\":\"00000000-0000-0000-0000-000000000000\",\"Message\":\"Test Message1\",\"FilePath\":";
+            Assert.That(_loggedMessages[0], Is.StringContaining(expectedMessageContains1));
+            Assert.That(_loggedMessages[0], Is.StringContaining(expectedMessageContains2));
 
         }
 
@@ -108,16 +137,75 @@ namespace LoggingSystemTests
             
             Assert.That(_loggedMessages[0], Is.StringContaining(TestMessage1));
 
-            string expectedMessage = String.Format("{{\"DateTime\":\"{0}\",\"Method\":\"{1}\",\"UserId\":\"00000000-0000-0000-0000-000000000000\",\"Message\":\"Test Message1\",\"Age\":\"9\",\"Year\":\"2014\"}}", TestDateTimeString, "Log_GivenMockPublisherWithParams_LogsCorrectMessage");
-            Assert.That(_loggedMessages[0], Is.EqualTo(expectedMessage));
+            string expectedMessageContains1 = String.Format("{{\"LogType\":\"Debug\",\"DateTime\":\"{0}\",\"Method\":\"{1}\",\"LineNumber\":", TestDateTimeString, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            string expectedMessageContains2 = "\"UserId\":\"00000000-0000-0000-0000-000000000000\",\"Message\":\"Test Message1\",\"Age\":\"9\",\"Year\":\"2014\",\"FilePath\":";
+            Assert.That(_loggedMessages[0], Is.StringContaining(expectedMessageContains1));
+            Assert.That(_loggedMessages[0], Is.StringContaining(expectedMessageContains2));
 
+        }
+
+        [Test]
+        public void Log_GivenMockPublisherWithException_LogsCorrectMessage()
+        {
+
+            //Act
+            _loggingSystm.Log(new Exception("TestException"), Logger, LoggingLevel.Error, TestMessage1, null);
+
+
+            //Assert
+            Assert.That(_loggedExceptionMessages.Count, Is.EqualTo(1), "Not expected number of logs");
+
+            Assert.That(_loggedExceptionMessages[0], Is.StringContaining(TestMessage1));
+
+            string expectedMessageContains1 = String.Format("{{\"LogType\":\"Error\",\"DateTime\":\"{0}\",\"Method\":\"{1}\",\"LineNumber\":", TestDateTimeString, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            const string expectedMessageContains2 = ",\"UserId\":\"00000000-0000-0000-0000-000000000000\",\"Message\":\"Test Message1\",\"Exception\":\"System.Exception: TestException\",\"FilePath\":";
+            Assert.That(_loggedExceptionMessages[0], Is.StringContaining(expectedMessageContains1));
+            Assert.That(_loggedExceptionMessages[0], Is.StringContaining(expectedMessageContains2));
+
+        }
+
+
+        [Test]
+        public void Log_GivenMockPublisherWithANullParamValue_LogsCorrectMessage()
+        {
+
+            //Act
+            _loggingSystm.Log(Logger, LoggingLevel.Debug, TestMessage1, new KeyValueCollection(new KeyValue("Age", null)));
+
+
+            //Assert
+            Assert.That(_loggedMessages.Count, Is.EqualTo(1), "Not expected number of logs");
+
+            Assert.That(_loggedMessages[0], Is.StringContaining(TestMessage1));
+
+            string expectedMessageContains1 =
+                String.Format("{{\"LogType\":\"Debug\",\"DateTime\":\"{0}\",\"Method\":\"{1}\",\"LineNumber\":", TestDateTimeString, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            const string expectedMessageContains2 = "\"UserId\":\"00000000-0000-0000-0000-000000000000\",\"Message\":\"Test Message1\",\"Age\":null,\"FilePath\":";
+            Assert.That(_loggedMessages[0], Is.StringContaining(expectedMessageContains1));
+            Assert.That(_loggedMessages[0], Is.StringContaining(expectedMessageContains2));
+        }
+
+
+        [Test]
+        public void Log_GivenMockPublisherWithParams_DoesNotLogWhenLevelDisabled()
+        {
+            _isLogEnabled = false;
+
+            //Act
+            _loggingSystm.Log(Logger, LoggingLevel.Debug, TestMessage1, null);
+
+            //Assert
+            Assert.That(_loggedMessages.Count,Is.EqualTo(0),"Not expected number of logs");
         }
 
 
 
         public override bool IsLogLevelEnabled
         {
-            get { return true; }
+            get
+            {
+                return _isLogEnabled;
+            }
         }
 
         public override void LogMessage(string message)
@@ -127,7 +215,24 @@ namespace LoggingSystemTests
 
         public override void LogException(string message, Exception ex)
         {
-            throw new NotImplementedException();
+            _loggedExceptionMessages.Add(message);
         }
+
+        private Exception CreateNestedException()
+        {
+            var exceptionSimulator = new TestExceptionThrowClass();
+
+            try
+            {
+                exceptionSimulator.ThrowNestedException();
+
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+            throw new NotImplementedException("Should never get ths");
+        }
+
     }
 }
---
+++ LoggingSystemTests/TestExceptionThrowClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace LoggingSystemTests
+{
+    public class TestExceptionThrowClass
+    {
+
+        public void ThrowNestedException()
+        {
+            RaiseDoubleNestedException();
+        }
+
+        private void RaiseDoubleNestedException()
+        {
+            try
+            {
+                RaiseNestedException();
+            }
+            catch (Exception ex)
+            {
+                throw new TargetException("I Are Top Outer Exception", ex);
+            }
+        }
+
+        private void RaiseNestedException()
+        {
+            try
+            {
+                MethodThenThrowException();
+            }
+            catch (Exception ex)
+            {
+                throw new TargetException("I Are Middle Exception", ex);
+            }
+        }
+
+        private void MethodThenThrowException()
+        {
+            ThrowException();
+        }
+
+        public void ThrowException()
+        {
+            throw new SuccessException("I Are Inner Raised Exception");
+        }
+
+
+    }
+}
